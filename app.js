fd0c4428cb1737ad9ec3ea3d08245ce135976074
etc276 <etc276@gmail.com>
2018-07-15 12:28:54 +0800

    Modify code section in app.js, change port listening to 8081

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var app = express();
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-// author: lary
-var fs = require("fs");
+// import module
 var bodyParser = require('body-parser');
-var querystring = require('querystring');
-var http = require('http');
-var util=require('./models//Utility');
-var crypt = require('./models/usage');
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var express = require('express');
 var filesock = require('./models/filesock');
+var io = require('socket.io');
+var logger = require('morgan');
+var path = require('path');
 var session = require('express-session')( {
     secret:'XASDASDA',
     resave: true,
     saveUninitialized: true
 });
 var sharedsession = require("express-socket.io-session");
-var io = require('socket.io'); 
-var ssn;
 
-var server = app.listen(8081, function () { // change port from 8081 to 3000
-	var host = server.address().address
-	var port = server.address().port
-	console.log("Server start http://%s:%s", host, port)
-});
-var serv_io = io.listen(server);
+// express app create
+var app = express();
 
+// environment setup
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session);
-serv_io.use(sharedsession(session, {
-    autoSave:true
-}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
-    next();
-});
-
-serv_io.sockets.on('connection', function(socket) {
-	filesock.sock_recv(socket);
-});	
-
-var indexRouter = require('./controllers/index');
-var creatorsRouter = require('./controllers/creators');
-var usersRouter = require('./controllers/users');
-var loginRouter = require('./controllers/login');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8081);
+
+// router setup
+var indexRouter = require('./controllers/index');
+var creatorsRouter = require('./controllers/creators');
+var usersRouter = require('./controllers/users');
+var loginRouter = require('./controllers/login');
 app.use('/', indexRouter);
 app.use('/creators', creatorsRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+
+// settting header
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -75,13 +59,28 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+// server port and io
+var server = app.listen(8081, function () {
+	let host = server.address().address
+	let port = server.address().port
+	console.log("Server start http://%s:%s", host, port)
+});
+
+var serv_io = io.listen(server);
+
+serv_io.use(sharedsession(session, { autoSave:true }));
+
+serv_io.sockets.on('connection', function(socket) {
+	filesock.sock_recv(socket);
 });
 
 module.exports = app;
