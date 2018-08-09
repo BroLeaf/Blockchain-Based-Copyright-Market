6c65acd78f8c4d735d4d43bff068479fd124a143
etc276 <etc276@gmail.com>
2018-08-09 10:33:11 +0800

    Rewrite solidity contract

--- data/contracts/simpleContract.sol
+++ data/contracts/simpleContract.sol
@@ -5,34 +5,14 @@ contract Receiver {
     address constant public clientA = 0x123;
     address constant public clientB = 0x456;
 
-    // construtor 
-    function Receiver() public {
-        
-    }
-    
-    // send
-    function payMe() payable public {
-        clientA.call.value(1000).gas(20317)();
-        clientB.call.value(1000).gas(20317)();
-    }
     
     // fallback payable
     function () payable public {
-        if(msg.value < 10000)
-            throw;
-        payMe();
-        // address(this).transfer(msg.value);
-        // msg.sender.transfer(msg.value);
-        // balances[msg.sender] -= msg.value;
-        // balances[this] += msg.value;
-    }
+        
+        require(msg.sender.balance >= msg.value);
+        require(msg.sender.balance >= 10000);
 
-    // get balances
-    function balanceOf(address _owner) public constant returns (uint256) {
-        return _owner.balance;
-    }
-    
-    function balanceOfContract() public constant returns (uint256) {
-        return address(this).balance;
+        clientA.transfer(msg.value / 2);
+        clientB.transfer(msg.value / 2);
     }
 }
